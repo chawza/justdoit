7cfd2724e3eadf81c1135a474ca22c8a04a530d3
NabeelKM <nabeelkahlil403@gmail.com>
2020-12-03 21:40:38 +0700

    create shoes and transactions table

---
+++ database/migrations/2020_12_03_143445_create_transaction_detail_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_detail', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id');
+            $table->foreignId('shoe_id');
+            $table->double('price', 8, 2);
+            $table->integer('num_items');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_detail');
+    }
+}
