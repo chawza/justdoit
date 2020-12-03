7cb87e9c16b081961ceed83ebbf329f84f6d35f7
NabeelKM <nabeelkahlil403@gmail.com>
2020-12-04 06:09:43 +0700

    added URL routing for store and transactions

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+# Route::redirect('/', '/login/login'); # replace after login function is active
+
+# URL : /login/login
+// Route::group('login')->group(function(){
+//     Route::get('login', '');
+//     Route::get('register', '');
+// });
+
+# URL : /store/showcase
+Route::prefix('store')->group(function(){
+    Route::get('showcase', 'Store@storeShowcase');
+    Route::get('shoe', 'Store@showDetail');
+    Route::get('addShoe', 'Store@addShoe');
+    Route::get('update', 'Store@updateShoeDetail');
+});
+
+#URL : /transaction/cart
+Route::prefix('transaction')->group(function(){
+   Route::get('cart', 'Store@cartListDetail');
+   Route::get('transactions', 'Store@viewTransactions');
+});
