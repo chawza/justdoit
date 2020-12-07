a9d19549eb2593e4e0c22c7d626dcc48fdb4ec2f
NabeelKM <nabeelkahlil403@gmail.com>
2020-12-07 13:51:58 +0700

    shoe detil page has been added

---
+++ resources/views/shoe.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.store')
+
+@section('main_panel')
+    <div class="row">
+        <div class="col-4">
+            <img class="img-fluid" src="{{ asset('storage/img/' . $item->thumbnail) }}" alt="">
+        </div>
+        <div class="col-8">
+            <h5>{{ $item->name }}</h5>
+            <p>Rp{{ number_format($item->price, 2, ',', '.') }}</p>
+            <p>Available : {{ $item->quantity }}</p>
+            <p>{{ $item->description }}</p>
+
+            @if ($user->role == 'admin')
+            <a href="/store/update/{{ $item->id }}" class="btn btn-primary">Update</a>
+            @else
+                <form action="/store/shoe" method="post">
+                    @csrf
+                    <label for="quantity">Item Quantity</label>
+                    <input type="number" name="quantity" min="1" max="{{$item->quantity}}">
+
+                    <input type="hidden" name="item_id" value="{{$item->id}}">
+                    <button type="submit" class="btn btn-primary" value="Submit">Add to Cart</button>
+                </form>
+            @endif
+        </div>
+    </div>
+@endsection
